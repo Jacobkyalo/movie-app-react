28a0d34b18290f8e1e942febcfbdd6beb06f864e
Jacob <jacobkyalo43@gmail.com>
2022-07-16 14:39:15 +0300

    mui Buttons ,Icons and styles

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,12 +1,32 @@
-import React from "react";
-import { TextField } from "@mui/material";
+import React, { useState } from "react";
+import { Button } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 import "./SearchBox.css";
 
 const SearchBox = () => {
+  const [showSearchBox, setShowSearchBox] = useState(false);
+
   return (
     <div className="search">
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <button>Search</button>
+      <div className="search__area">
+        {showSearchBox && (
+          <div>
+            <input type="text" className="search__box" />
+            <Button variant="contained" sx={{ marginLeft: "10px" }}>
+              Search
+            </Button>
+          </div>
+        )}
+        <SearchIcon
+          sx={{
+            fontSize: "35px",
+            alignItems: "center",
+            marginLeft: "10px",
+            cursor: "pointer",
+          }}
+          onClick={() => setShowSearchBox(!showSearchBox)}
+        />
+      </div>
     </div>
   );
 };
