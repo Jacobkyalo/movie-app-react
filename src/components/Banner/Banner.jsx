bf376e7a92ef7c389dbe46eeecd0904936ab9faf
Jacob <jacobkyalo43@gmail.com>
2022-07-17 15:43:44 +0300

    config add

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./Banner.css";
+import { image_url } from "../../config/config";
 
-const Banner = () => {
+const Banner = ({ media }) => {
   const [banner, setBanner] = useState();
 
   const fetchBannerDetails = async () => {
     const res = await axios.get(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&with_genres=28`
+      `https://api.themoviedb.org/3/discover/${
+        media ? "tv" : "movie"
+      }?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&with_genres=28`
     );
     setBanner(
       res.data.results[Math.floor(Math.random() * res.data.results.length - 1)]
@@ -19,11 +22,16 @@ const Banner = () => {
     fetchBannerDetails();
   }, []);
 
+  //truncate function
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
+
   return (
     <section
       className="banner"
       style={{
-        backgroundImage: `url(https://image.tmdb.org/t/p/original/${banner?.backdrop_path})`,
+        backgroundImage: `url(${image_url}/${banner?.backdrop_path})`,
         backgroundPosition: "center center",
         backgroundSize: "cover",
       }}
@@ -36,7 +44,7 @@ const Banner = () => {
               banner?.original_name ||
               banner?.name}
           </h3>
-          <p className="banner__overview">{banner.overview}</p>
+          <p className="banner__overview">{truncate(banner.overview, 200)}</p>
         </div>
       )}
     </section>
---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+export const image_url = "https://image.tmdb.org/t/p/original";
+
+export const no_poster_img =
+  "https://www.movienewz.com/img/films/poster-holder.jpg";
