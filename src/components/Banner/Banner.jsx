5f4358acec96387eb5f3a0a98dd854fab73a3a24
Jacob <jacobkyalo43@gmail.com>
2022-07-17 13:32:27 +0300

    banner images

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./Banner.css";
 
 const Banner = () => {
+  const [banner, setBanner] = useState();
+
+  const fetchBannerDetails = async () => {
+    const res = await axios.get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&with_genres=28`
+    );
+    setBanner(
+      res.data.results[Math.floor(Math.random() * res.data.results.length - 1)]
+    );
+    //console.log(res.data.results);
+  };
+
+  useEffect(() => {
+    fetchBannerDetails();
+  }, []);
+
   return (
-    <section className="banner">
-      <div className="banner__content">
-        <h3 className="banner__title">Gemini Man</h3>
-        <p className="banner__overview">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum,
-          fugiat est? Assumenda, alias quis? Beatae sint fugit deleniti
-          similique obcaecati assumenda et distinctio, dolore nisi consectetur,
-          voluptates nesciunt fugiat ex.
-        </p>
-      </div>
+    <section
+      className="banner"
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/original/${banner?.backdrop_path})`,
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+      }}
+    >
+      {banner && (
+        <div className="banner__content">
+          <h3 className="banner__title">
+            {banner?.original_title ||
+              banner?.title ||
+              banner?.original_name ||
+              banner?.name}
+          </h3>
+          <p className="banner__overview">{banner.overview}</p>
+        </div>
+      )}
     </section>
   );
 };
--- src/components/Genres/Genres.jsx
+++ src/components/Genres/Genres.jsx
@@ -3,35 +3,32 @@ import React, { useEffect, useState } from "react";
 import "./Genres.css";
 
 const Genres = () => {
-  const [genres, setGenres] = useState();
+  const [genres, setGenres] = useState([]);
 
   const fetchGenres = async () => {
     const data = await axios.get(
       `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
     );
     setGenres(data.data.genres);
-    //console.log(data.data.genres);
   };
 
   useEffect(() => {
     fetchGenres();
-
-    return () => {
-      setGenres({});
-    };
   }, []);
 
   return (
     <>
       <h2 className="title">Genres</h2>
       {genres && (
-        <div className="genres">
-          {genres.map((genre) => (
-            <p className="genre" key={genre.id}>
-              {genre.name}
-            </p>
-          ))}
-        </div>
+        <section className="genres">
+          <ul className="genres__list">
+            {genres.map((genre) => (
+              <li className="genre" key={genre.id}>
+                {genre.name}
+              </li>
+            ))}
+          </ul>
+        </section>
       )}
     </>
   );
