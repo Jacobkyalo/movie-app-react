cec8e553f46432dfdd1bdb77aa7a0fd48886d990
Jacob <jacobkyalo43@gmail.com>
2022-07-23 10:24:26 +0300

    updated SearchBox component

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -2,20 +2,22 @@ import React from "react";
 import { Button } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import "./SearchBox.css";
+import { Link } from "react-router-dom";
 
 const SearchBox = () => {
   return (
-    <div className="search">
-      <div className="search__area">
-        <input type="text" className="search__box" />
-        <Button
-          variant="contained"
-          sx={{ marginLeft: "3px", backgroundColor: "red" }}
-        >
-          <SearchIcon />
-        </Button>
+    <>
+      <div className="search__btn">
+        <Link to="/search">
+          <Button
+            variant="contained"
+            sx={{ marginLeft: "3px", backgroundColor: "red" }}
+          >
+            <SearchIcon />
+          </Button>
+        </Link>
       </div>
-    </div>
+    </>
   );
 };
 
